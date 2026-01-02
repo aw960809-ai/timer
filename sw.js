f046cc87d147a7cafd2b3600ad54c431e47200e6
aw960809-ai <aw960809@gmail.com>
2026-01-02 10:01:44 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = "timer-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.css?v=1",
+  "./app.js?v=1",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k)))))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
